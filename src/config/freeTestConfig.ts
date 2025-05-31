46ac64211d7904ded651b09bea62b76e62d0ecaa
Stanley Ng <stanley121499@gmail.com>
2025-05-31 12:04:45 +0800

    ye

--- src/config/freeTestConfig.ts
+++ src/config/freeTestConfig.ts
@@ -4,7 +4,7 @@
  */
 const FREE_TEST_CONFIG = {
   // Set the end date for the free test feature (YYYY-MM-DD format)
-  endDate: "2025-05-23",
+  endDate: "2025-06-05",
   // Feature control flag - can be set to false to immediately disable the feature
   isEnabled: true
 };
